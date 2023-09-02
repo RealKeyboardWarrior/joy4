d4a03294cd7110424366eb0285ab13805cc67b20
KeyboardWarrior <contact@keyboardwarrior.be>
2023-09-02 21:44:41 +0200

    fix(h264parser): extract AVCC

---
+++ codec/h264parser/avcc.go
@@ -0,0 +1,44 @@
+package h264parser
+
+import (
+	"fmt"
+
+	"github.com/RealKeyboardWarrior/joy4/utils/bits/pio"
+)
+
+var ErrInvalidAVCC = fmt.Errorf("bytestream b was not AVCC")
+
+func DecodeAVCC(b []byte) ([][]byte, error) {
+	if len(b) < 4 {
+		return nil, ErrInvalidAVCC
+	}
+
+	val4 := pio.U32BE(b)
+	if val4 > uint32(len(b)-4) {
+		return nil, ErrInvalidAVCC
+	}
+
+	_val4 := val4
+	_b := b[4:]
+	nalus := [][]byte{}
+	for {
+		nalus = append(nalus, _b[:_val4])
+		if _val4 > uint32(len(_b)) {
+			break
+		}
+		_b = _b[_val4:]
+		if len(_b) < 4 {
+			break
+		}
+		_val4 = pio.U32BE(_b)
+		_b = _b[4:]
+		if _val4 > uint32(len(_b)) {
+			break
+		}
+	}
+	if len(_b) == 0 {
+		return nalus, nil
+	}
+
+	return nil, ErrInvalidAVCC
+}
